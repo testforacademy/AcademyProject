82c53cd9facfae974629e99634908cf16a8d0a29
Max Goncharuk <goncharuk.m.n@gmail.com>
2014-07-23 11:59:42 +0300

    2: Добавление метода сложения двух чисел

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -1,9 +1,17 @@
-﻿namespace Calculator
+﻿using System;
+
+namespace Calculator
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(AddTwoNumber(2, 2));
+        }
+
+        public static double AddTwoNumber(double x, double y)
+        {
+            return x + y;
         }
     }
 }
