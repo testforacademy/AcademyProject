0c7ebd92d3c5484bbc2bff58acf9dcad6cd3ab5d
Max Goncharuk <goncharuk.m.n@gmail.com>
2014-07-23 12:42:09 +0300

    7: Добавил метод деления чисел

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -8,6 +8,7 @@ namespace Calculator
         {
             Console.WriteLine(AddTwoNumbers(2, 2));
             Console.WriteLine(SubTwoNumbers(5, 3));
+            Console.WriteLine(DivideTwoNumber(10, 2));
         }
 
         public static string AddTwoNumbers(double x, double y)
@@ -18,6 +19,13 @@ namespace Calculator
         public static string SubTwoNumbers(double x, double y)
         {
             return (x - y).ToString();
+        }
+
+        public static string DivideTwoNumber(double x, double y)
+        {
+            if( y == 0)
+                throw new DivideByZeroException();
+            return (x/y).ToString();
         }
     }
 }
